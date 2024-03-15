65d0c48320e14544bd30a8261c8c2f86e45cc4f5
disrin <162717678+disrin@users.noreply.github.com>
2024-03-15 09:05:45 -0400

    Update binary

--- make_binaryFiles.py
+++ make_binaryFiles.py
@@ -1,13 +1,13 @@
 """
-This script processes 'bottom 1 percent' distance matrix files by converting all non-zero values to 1 and keeping zero values unchanged, thus creating a binary matrix. 
-This transformation is particularly useful for emphasizing significant entries in the matrix while reducing file size and simplifying analysis. The resulting binary 
+This script processes 'bottom 1 percent' distance matrix files by converting all non-zero values to 1 and keeping zero values. 
+This is useful for emphasizing significant values in the matrix while reducing file size and simplifying analysis. The resulting binary 
 matrices are saved as integer values.
 
-Each file processed is saved with a 'binary_' prefix facilitating easy identification. 
+Each file processed is saved with a 'binary_' prefix. 
 
 To use the script:
-- Set 'input_dir' to the directory containing your 'bottom 1 percent' files.
-- Set 'output_dir' to the directory where you want the binary files saved.
+- Set 'input_dir' to directory containing 'bottom 1 percent' files.
+- Set 'output_dir' to directory where you want the binary files saved.
 """
 
 import numpy as np
@@ -18,13 +18,13 @@ def convert_to_binary_and_save(input_dir, output_dir=None):
     if output_dir and not os.path.exists(output_dir):
         os.makedirs(output_dir, exist_ok=True)
     elif not output_dir:
-        output_dir = input_dir  # Default to input directory if output directory not specified
+        output_dir = input_dir  # If input directory not specified use output directory
 
     for file_path in glob.glob(os.path.join(input_dir, 'bottom1p_*.txt')):
         print(f"Processing {file_path}")
         matrix = np.loadtxt(file_path)
         
-        binary_matrix = (matrix > 0).astype(int)  # Convert non-zero to 1, keep zero as 0, ensure int type
+        binary_matrix = (matrix > 0).astype(int)  # Convert non-zero to 1, keep zero as 0, int type
         
         file_name = os.path.basename(file_path)
         output_file_path = os.path.join(output_dir, f"binary_{file_name}")  # Prefix output with 'binary_'
@@ -35,6 +35,6 @@ def convert_to_binary_and_save(input_dir, output_dir=None):
 # set input and output path
 
 input_dir = 'path/to/your/input/files'  # Directory containing 'bottom 1 percent' files
-output_dir = 'path/to/your/output/files'  # Output directory for binary files; can be the same as input_dir
+output_dir = 'path/to/your/output/files'  # Output directory for binary files
 
 convert_to_binary_and_save(input_dir, output_dir)
